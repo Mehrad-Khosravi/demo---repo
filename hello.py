d918ddfce0a00334563624bcc41a8f9036bfe3a7
Mehrad27 <mehrad.khosravi79@gmail.com>
2024-07-03 12:40:48 +0330

    description of algorithm
    What is RL and Q-Learning?

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This code says hello to everyone
+
+def main():
+    print(hello())
+
+def hello():
+    return "Hello "+input("Enter yout name: ")+"!"
+
+
+if __name__ == "__main__":
+    main()
